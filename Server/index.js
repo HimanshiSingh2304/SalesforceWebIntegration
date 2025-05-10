d0a0358536246dab78305cd472a97c70b59c2d8b
Himanshi Singh <97846576+HimanshiSingh2304@users.noreply.github.com>
2025-05-10 10:47:59 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({ 
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'https://salesforcewebintegrationfrontend.onrender.com', 
   credentials: true 
 }));
 app.use(express.json());
